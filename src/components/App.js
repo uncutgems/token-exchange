cf2d86db38de8bc04f3342ba37c59427815a4e31
unknown <qd01705@gmail.com>
2021-06-16 15:41:16 -0400

    Complete project config for local blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,142 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+import Token from '../abis/Token.json';
+import EthSwap from '../abis/EthSwap.json'
+import Navbar from './NavBar';
+import Main from './Main';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+	async componentWillMount() {
+		await this.loadWeb3();
+		await this.loadBlockchainData();
+	}
 
+	async loadBlockchainData() {
+		const web3 = window.web3;
+
+		// get account
+		const accounts = await web3.eth.getAccounts();
+		this.setState({ account: accounts[0] });
+
+		// Set EthSwap balance
+		const ethBalance = await web3.eth.getBalance(this.state.account);
+		this.setState({ ethBalance });
+
+		// Load Network ID
+		const networkId = await web3.eth.net.getId();
+
+		// Load Token
+		const tokenData = Token.networks[networkId];
+		if (tokenData) {
+			const token = new web3.eth.Contract(Token.abi, tokenData.address);
+			this.setState({ token });
+			let tokenBalance = await token.methods.balanceOf(this.state.account).call();
+      console.log(token.methods);
+      {debugger}
+			this.setState({ tokenBalance: tokenBalance.toString() });
+		} else {
+			window.alert("Token Network not detected");
+		}
+
+		// Load EthSwap
+		const ethSwapData = EthSwap.networks[networkId];
+		if (ethSwapData) {
+			const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+			this.setState({ ethSwap });
+		} else {
+			window.alert("EthSwap Network not detected");
+		}
+
+		// Loading is done, to set loading == false
+		this.setState({ loading: false });
+	}
+
+	async loadWeb3() {
+		// Modern dapp browsers...
+		if (window.ethereum) {
+			window.web3 = new Web3(window.ethereum);
+			await window.ethereum.enable();
+		}
+		// Legacy dapp browsers...
+		else if (window.web3) {
+			window.web3 = new Web3(window.web3.currentProvider);
+		}
+		// Non-dapp browsers...
+		else {
+			window.alert("Non-Ethereum browser detected. You should consider trying MetaMask!");
+		}
+	}
+
+	// Buy tokens @desc take input some amount of wei
+	buyTokens = async (etherAmount) => {
+		this.setState({ loading: true });
+		this.state.ethSwap.methods
+			.buyTokens()
+			.send({ value: etherAmount, from: this.state.account })
+			.on("transactionHash", (hash) => {
+				this.setState({ loading: false });
+			});
+	};
+
+	sellTokens = async (tokenAmount) => {
+		this.setState({ loading: true });
+		this.state.token.methods
+			.approve(this.state.ethSwap.address, tokenAmount)
+			.send({ from: this.state.account })
+			.on("transactionHash", (hash) => {
+				this.state.ethSwap.methods
+					.sellTokens(tokenAmount)
+					.send({ from: this.state.account })
+					.on("transactionHash", (hash) => {
+						this.setState({ loading: false });
+					});
+			});
+	};
+
+	// react state
+	constructor(props) {
+		super(props);
+		this.state = {
+			account: "",
+			token: {},
+			ethSwap: {},
+			tokenBalance: "0",
+			ethBalance: "0",
+			loading: true,
+		};
+	}
+
+	render() {
+		let content;
+		if (this.state.loading)
+			content = (
+				<p id="loader" className="text-center">
+					Loading...
+				</p>
+			);
+		else
+			content = (
+				<Main
+					ethBalance={this.state.ethBalance}
+					tokenBalance={this.state.tokenBalance}
+					buyTokens={this.buyTokens}
+					sellTokens={this.sellTokens}
+				/>
+			);
+		return (
+			<div>
+				<Navbar account={this.state.account} />
+				<div className="container-fluid mt-5">
+					<div className="row">
+						<main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: "600px" }}>
+							<div className="content mr-auto ml-auto">{content}</div>
+						</main>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 export default App;
---
+++ src/components/SellForm.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import Web3 from 'web3';
+import ethLogo from '../eth-icon.png';
+import qoin from '../qoin.png';
+
+class SellForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: '0',
+        }
+    }
+    render() {
+        const web3 = new Web3(window.ethereum).eth;
+        return (
+            <form className="mb-3" onSubmit={(event) => {
+                event.preventDefault();
+                let tokenAmount = this.input.value.toString();
+                tokenAmount = web3.utils.toWei(tokenAmount, 'Ether');
+                this.props.sellTokens(tokenAmount);
+            }
+            }>
+                <div>
+                    <label className="float-left">
+                        <b>Input</b>
+                    </label>
+                    <span className="float-right text-muted">
+                        Balance: {web3.utils.fromWei(this.props.tokenBalance, 'Ether')}
+                    </span>
+                </div>
+                <div className="input-group mb-4">
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="0"
+                        onChange={() => {
+                            this.setState({
+                                output: (this.input.value / 100).toString(),
+                            })
+                        }}
+                        ref={(input) => { this.input = input }}
+                        required
+                    />
+                    <div className="input-group-append">
+                        <div className="input-group-text">
+                            <img src={qoin} height='32' alt="" />
+                                    &nbsp;&nbsp;&nbsp; QOIN
+                                </div>
+                    </div>
+                </div>
+                <div>
+                    <label className="float-left"><b>Output</b></label>
+                    <span className="float-right text-muted">
+                        Balance: {web3.utils.fromWei(this.props.ethBalance, 'Ether')}
+                    </span>
+                </div>
+                <div className="input-group mb-2">
+                    <input
+                        type="text"
+                        className="form-control form-control-lg"
+                        placeholder="0"
+                        value={this.state.output}
+                        disabled
+                    />
+                    <div className="input-group-append">
+                        <div className="input-group-text">
+                            <img src={ethLogo} height='32' alt="" />
+                                    &nbsp; ETH
+                                </div>
+                    </div>
+                </div>
+                <div>
+                    <span className="float-left text-muted">Exchange Rate</span>
+                    <span className="float-right text-muted">100 QOIN = 1 ETH</span>
+                </div>
+                <button type="submit" className="btn btn-primary btn-block btn-lg">EXCHANGE!</button>
+            </form>
+
+        );
+    }
+}
+
+export default SellForm;
